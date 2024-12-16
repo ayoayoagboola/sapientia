1b2170ec7ecb63948967f958499779343bad9ae7
ayoagboola <ayoagboola484@gmail.com>
2024-12-15 19:16:20 -0600

    TODO: edit some styles 🌳

--- components/ui/sonner.tsx
+++ components/ui/sonner.tsx
@@ -1,23 +1,23 @@
-"use client"
+"use client";
 
 import { LoaderCircle, SquareCheck, TriangleAlert } from "lucide-react";
-import { useTheme } from "next-themes"
-import { Toaster as Sonner } from "sonner"
+import { useTheme } from "next-themes";
+import { Toaster as Sonner } from "sonner";
 
-type ToasterProps = React.ComponentProps<typeof Sonner>
+type ToasterProps = React.ComponentProps<typeof Sonner>;
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "light" } = useTheme()
+  const { theme = "light" } = useTheme();
 
   return (
     <Sonner
       theme={theme as ToasterProps["theme"]}
-      className="bg-slate-50 text-sm group toast relative flex w-full items-center justify-start space-x-2 overflow-hidden rounded-md border p-4 pr-6 shadow-lg transition-all"
+      className="toaster group"
       toastOptions={{
         unstyled: true,
         classNames: {
           toast:
-            "group toast group-[.toaster]:bg-slate-50 group-[.toaster]:text-slate-900 group-[.toaster]:border-slate-900",
+            "bg-slate-50 text-sm group toast relative fixed bottom-4 right-4 z-50 flex w-full items-center justify-start gap-3 overflow-hidden rounded-lg border border-slate-900 py-3 px-4 transition-all",
           description: "group-[.toast]:text-slate-500",
           actionButton:
             "group-[.toast]:bg-slate-900 group-[.toast]:text-slate-50",
@@ -34,7 +34,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
       }}
       {...props}
     />
-  )
-}
+  );
+};
 
-export { Toaster }
+export { Toaster };
